9ddbc46037903c8be3773731880bdeffb4d35cd5
Acaz Souza <acazsouza@gmail.com>
2026-01-21 19:26:21 -0300

    asgasg

--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
 import { serveFile } from "jsr:@std/http/file-server";
-
-Deno.serve((req: Request) => {
-const url = new URL(req.url);
+Deno.serve(async (req) => {
+    const url = new URL(req.url);
     const uri = url.searchParams.get("uri");
     
     if (uri) {
-        await fetch(uri, { method: "POST" });
+        fetch(uri, { method: "POST" });
     }
     
     return serveFile(req, "./index.html");
