34e2a8e5e64375b716d49ba14ecb9dd583bc81ac
Acaz Souza <acazsouza@gmail.com>
2026-01-21 19:24:56 -0300

    refac

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
 import { serveFile } from "jsr:@std/http/file-server";
 
 Deno.serve((req: Request) => {
+const url = new URL(req.url);
+    const uri = url.searchParams.get("uri");
+    
+    if (uri) {
+        await fetch(uri, { method: "POST" });
+    }
+    
     return serveFile(req, "./index.html");
 });
